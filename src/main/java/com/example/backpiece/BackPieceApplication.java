edb7bf974e50f27ed178f2b351d3e78f0df6e126
NikGan <nik.ganenko@mail.ru>
2024-05-02 21:56:40 +0400

    Test

---
+++ src/main/java/com/example/backpiece/BackPieceApplication.java
@@ -0,0 +1,13 @@
+package com.example.backpiece;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackPieceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackPieceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backpiece/BackPieceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backpiece;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackPieceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
